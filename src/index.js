1b00d5de79062275990dd0a77c9b71eb223ce000
NandniParekh <168457292+NandniParekh@users.noreply.github.com>
2025-11-09 14:49:46 +0530

    Finalizing React frontend structure and removing embedded backend repository

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
+// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './style.css'; // 👈 ENSURE YOUR CSS FILE IS NAMED 'style.css' AND IS IN THE 'src' FOLDER
+import { BrowserRouter } from 'react-router-dom'; // Import Router
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter> {/* Wrapper required for Link and useNavigate to work */}
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
