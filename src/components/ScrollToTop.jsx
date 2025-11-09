d8fcc1abe229d7797417aa55c8cf10afcae4430e
Sachchiyay-Exports <sachchiyayexports@gmail.com>
2025-11-10 01:24:54 +0530

    Update ScrollToTop.jsx

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -1,16 +1,16 @@
-// src/components/ScrollToTop.jsx
-import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+// // src/components/ScrollToTop.jsx
+// import { useEffect } from 'react';
+// import { useLocation } from 'react-router-dom';
 
-const ScrollToTop = () => {
-  const { pathname } = useLocation();
+// const ScrollToTop = () => {
+//   const { pathname } = useLocation();
 
-  useEffect(() => {
-    // FIX: Forces the window to scroll to the top (0, 0) whenever the URL path changes
-    window.scrollTo(0, 0);
-  }, [pathname]); 
+//   useEffect(() => {
+//     // FIX: Forces the window to scroll to the top (0, 0) whenever the URL path changes
+//     window.scrollTo(0, 0);
+//   }, [pathname]); 
 
-  return null;
-};
+//   return null;
+// };
 
-export default ScrollToTop;
+// export default ScrollToTop;
